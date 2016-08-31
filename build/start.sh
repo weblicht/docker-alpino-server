9ba6e815438a69cbf32850ca10220ca57eceafc0
Wei Qiu <wei.qiu@uni-tuebingen.de>
2016-08-31 17:39:05 +0200

    Use supervisord to manage multiple processes

--- build/start.sh
+++ build/start.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-echo "Starting the parsing server..."
-make -f /Alpino/Makefile.start_server
-echo "Done!"
 SCRIPTDIR=$ALPINO_HOME/Tokenization/tokenize_server
 
 export LD_LIBRARY_PATH=${SCRIPTDIR}/..
 
 echo "Starting the tokenizer server..."
-${SCRIPTDIR}/tokenize_server &> /tokenizer.log &
-echo "Done!"
+${SCRIPTDIR}/tokenize_server 
